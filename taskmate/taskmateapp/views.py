dea308be1b08b9e398fd3a40fd5bbd120f981cf7
dhara-maru <dharamaru406@gmail.com>
2024-09-20 19:02:42 +0530

    Displayed all emps on all_emp.html from DB

--- taskmate/taskmateapp/views.py
+++ taskmate/taskmateapp/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 
+from .models import Employee
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def all_emp(request):
-    return render(request, 'all_emp.html')
+    emps = Employee.objects.all()
+    context = {
+        'emps' : emps
+    }
+    print(context)
+    return render(request, 'all_emp.html',context)
 
 def add_emp(request):
     return render(request, 'add_emp.html')
