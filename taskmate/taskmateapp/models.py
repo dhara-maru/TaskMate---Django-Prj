6ee0f8a130d4cb0b06c9c02101c5c84c7e3bff21
dhara-maru <dharamaru406@gmail.com>
2024-09-20 18:15:05 +0530

    Inserted Data in Employee, Dept and Roles

--- taskmate/taskmateapp/models.py
+++ taskmate/taskmateapp/models.py
@@ -6,8 +6,15 @@ class Department(models.Model):
     name = models.CharField(max_length=30, null = False)
     location = models.CharField(max_length=30)
     
+    #to show the data in admin panel
+    def __str__(self):
+        return self.name
+    
 class Role(models.Model):
     name = models.CharField(max_length=100, null=False)
+    def __str__(self):
+        return self.name
+    
     
 class Employee(models.Model):
     first_name = models.CharField(max_length=30, null = False)
@@ -19,3 +26,5 @@ class Employee(models.Model):
     phone = models.IntegerField(default=0)
     hire_date = models.DateField()
     
+    def __str__(self):
+        return "%s %s %s" %(self.first_name, self.last_name, self.phone)
