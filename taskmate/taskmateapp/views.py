aa51c299df30ff651d66bfa6e58ab7701da0c4d0
dhara-maru <dharamaru406@gmail.com>
2024-09-22 10:02:50 +0530

    Implemented Dropdowns for Department and Role inputs.

--- taskmate/taskmateapp/views.py
+++ taskmate/taskmateapp/views.py
@@ -16,39 +16,28 @@ def all_emp(request):
     return render(request, 'all_emp.html',context)
 
 def add_emp(request):
-   
-
     if request.method == 'POST':
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
-        dept_id = request.POST.get('dept')  
-        salary = int(request.POST.get('salary', 0))
-        bonus = int(request.POST.get('bonus', 0))
-        role_id = int(request.POST.get('role', 0))
-        phone = int(request.POST.get('phone', 0))
-        hire_date = request.POST.get('hire_date')
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        dept_id = request.POST['dept']  # Getting selected department id
+        salary = int(request.POST['salary'])
+        bonus = int(request.POST['bonus'])
+        role_id = request.POST['role']  # Getting selected role id
+        phone = request.POST['phone']
+        hire_date = request.POST['hire_date']
 
-        
-        department = Department.objects.get(id=dept_id)
-        role = Role.objects.get(id=role_id)
+        dept = Department.objects.get(id=dept_id)  # Get Department instance by id
+        role = Role.objects.get(id=role_id)  # Get Role instance by id
 
-        newemp = Employee(
-            first_name=first_name,
-            last_name=last_name,
-            salary=salary,
-            bonus=bonus,
-            phone=phone,
-            hire_date=hire_date,
-            dept_id=department,  
-            role_id=role
-        )
-
+        newemp = Employee(first_name=first_name, last_name=last_name, dept_id=dept,
+                          salary=salary, bonus=bonus, role_id=role, phone=phone,
+                          hire_date=hire_date)
         newemp.save()
         return HttpResponse('Employee added successfully!')
-    elif request.method == 'GET':
-        return render(request, 'add_emp.html')
     else:
-        return HttpResponse('An exception occurred! Employee is not added.')
+        departments = Department.objects.all()  # Query all departments
+        roles = Role.objects.all()  # Query all roles
+        return render(request, 'add_emp.html', {'departments': departments, 'roles': roles})
     
     
 
